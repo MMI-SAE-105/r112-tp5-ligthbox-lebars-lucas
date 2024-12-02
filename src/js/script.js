b4799ba35ec0c08ab485435c90731a5c122b27d5
Lucas <lucas.lebars@edu.univ-fcomte.fr>
2024-12-02 15:29:27 +0100

    ajout js et css

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,12 @@
 // *** LIGHTBOX ***
+const LightboxImg = document.querySelector("#lightbox img");
+const lightBox = document.querySelector("#lightbox");
+
+document.querySelectorAll("[data-full-img]").forEach((imgThumb) => {
+    imgThumb.addEventListener("click", (evt) => {
+LightboxImg.src = imgThumb.dataset.fullImg;
+lightBox.showModal();
+    });
+});
+
+lightBox.addEventListener("click", (evt) =>  lightBox.close() );
