39e6e7b1c9dc9c5d6b6cc04765b4fac704308f1e
Lucas <lucas.lebars@edu.univ-fcomte.fr>
2024-12-04 14:41:03 +0100

    tp terminé

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,7 @@
 // *** LIGHTBOX ***
 const LightboxImg = document.querySelector("#lightbox img");
 const lightBox = document.querySelector("#lightbox");
+const closeBtn = document.querySelector("#close-lightbox");
 
 document.querySelectorAll("[data-full-img]").forEach((imgThumb) => {
     imgThumb.addEventListener("click", (evt) => {
@@ -9,4 +10,7 @@ lightBox.showModal();
     });
 });
 
-lightBox.addEventListener("click", (evt) =>  lightBox.close() );
+closeBtn.addEventListener("click", (evt) => {
+
+lightBox.close()
+    } );
